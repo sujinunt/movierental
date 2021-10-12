96c22fd86ec95b81b47aeb14f1c8d8d73c42c2ce
Sujinunt Puangmalai <sujinunt555@hotmail.com>
2021-10-12 18:05:51 +0700

    Remove Temp

--- customer.py
+++ customer.py
@@ -1,6 +1,6 @@
 from rental import Rental
 from movie import Movie
-import logging
+
 
 class Customer:
     """
@@ -17,10 +17,10 @@ class Customer:
     def add_rental(self, rental: Rental):
         if rental not in self.rentals:
             self.rentals.append(rental)
-    
+
     def get_name(self):
         return self.name
-    
+
     def statement(self):
         """
             Print all the rentals in current period, 
@@ -28,27 +28,34 @@ class Customer:
             Returns:
                 the statement as a String
         """
-        total_amount = 0 # total charges
-        frequent_renter_points = 0 
         statement = f"Rental Report for {self.name}\n\n"
         fmt = "{:32s}    {:4s} {:6s}\n"
         statement += fmt.format("Movie Title", "Days", "Price")
         fmt = "{:32s}   {:4d} {:6.2f}\n"
 
         for rental in self.rentals:
-            frequent_renter_points += rental.get_frequent_renter_points()
             #  add detail line to statement
             statement += fmt.format(rental.get_movie().get_title(), rental.get_days_rented(), rental.get_charge())
-            # and accumulate activity
-            total_amount += rental.get_charge()
 
         # footer: summary of charges
         statement += "\n"
         statement += "{:32s} {:6s} {:6.2f}\n".format(
-                       "Total Charges", "", total_amount)
-        statement += "Frequent Renter Points earned: {}\n".format(frequent_renter_points)
+                       "Total Charges", "", self.get_total_charge())
+        statement += "Frequent Renter Points earned: {}\n".format(self.get_total_frequent_renter_points())
 
         return statement
+
+    def get_total_charge(self):
+        total_amount = 0 # total charges
+        for rental in self.rentals:
+            total_amount += rental.get_charge()
+        return total_amount
+
+    def get_total_frequent_renter_points(self):
+        frequent_renter_points = 0
+        for rental in self.rentals:
+            frequent_renter_points += rental.get_frequent_renter_points()
+        return frequent_renter_points
 
 
 if __name__ == "__main__":
